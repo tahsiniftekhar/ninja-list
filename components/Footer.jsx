3cd66b684787215a1ca619ca1514d4be2190f11b
tahsiniftekhar <tahsiniftekhar@gmail.com>
2021-07-04 23:40:59 +0600

    initial commit

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <footer>
+            Copyright 2021 Ninja List
+        </footer>
+    )
+}
+
+export default Footer
---
+++ pages/about.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+const about = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="description" content="Ninja List"></meta>
+      </Head>
+      <div>
+        <h1>About Page</h1>
+      </div>
+    </>
+  );
+};
+
+export default about;
